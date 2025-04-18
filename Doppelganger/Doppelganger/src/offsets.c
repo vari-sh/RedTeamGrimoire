7db96aeba98082ecabb4615b86b6a70946b41192
vari-sh <15218409+vari-sh@users.noreply.github.com>
2025-04-18 08:34:48 +0200

    added offsets for windows 10 - server 2026

--- Doppelganger/Doppelganger/src/offsets.c
+++ Doppelganger/Doppelganger/src/offsets.c
@@ -13,27 +13,27 @@ Offsets getOffsets() {
     Offsets offs = { 0, 0, 0 };
 
     // Offsets table for x64 (TODO)
-    if (build <= 16384) {       // Windows 8 Server 2012 NO PPL FIELD
-        offs.ActiveProcessLinks = 0x0000;
-        offs.ImageFileName = 0x0000;
-        offs.Protection = 0x0000;
+    if (build == 14393) {           // Windows 10 / Server 2016
+        offs.ActiveProcessLinks = 0x2f0;
+        offs.ImageFileName = 0x450;
+        offs.Protection = 0x6c2;
     }
-    else if (build <= 17415) {       // Windows 8.1 Server 2012R2 and RTM (16384)
+    else if (build == 17415) {      // Windows 8.1 Server 2012R2 and RTM (16384)
         offs.ActiveProcessLinks = 0x2e8;
         offs.ImageFileName = 0x438;
         offs.Protection = 0x67a;
     }
-    else if (build <= 19045) {       // Windows 10 version 22H2 (Build 19041)
+    else if (build == 19045) {      // Windows 10 version 22H2 (Build 19041)
         offs.ActiveProcessLinks = 0x448;
         offs.ImageFileName = 0x5a8;
         offs.Protection = 0x87a;
     }
-    else if (build <= 22631) {  // Windows 23H2
+    else if (build == 22631) {      // Windows 11 23H2
         offs.ActiveProcessLinks = 0x448;
         offs.ImageFileName = 0x5a8;
         offs.Protection = 0x87a;
     }
-    else if (build > 22631) {  // Windows 11 24H2 and above
+    else if (build == 26100) {      // Windows 11 24H2 and above
         offs.ActiveProcessLinks = 0x1d8;
         offs.ImageFileName = 0x338;
         offs.Protection = 0x5fa;
